f0741df760305a7943ab1082df9ef15ced716440
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-14 23:24:22 +0530

    Model

--- FullStack/FullStack/src/main/java/com/kunal_debug_png/FullStack/model/User.java
+++ FullStack/FullStack/src/main/java/com/kunal_debug_png/FullStack/model/User.java
@@ -1,2 +1,26 @@
-package com.kunal_debug_png.FullStack.model;public class Use {
+package com.kunal_debug_png.FullStack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String name;
+    private String email;
 }
