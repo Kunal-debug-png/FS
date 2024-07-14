8107822854d039fc036bd4b4ea037a3f7aa9aff9
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-14 23:25:05 +0530

    Repo

--- FullStack/FullStack/src/main/java/com/kunal_debug_png/FullStack/repository/UserRepository.java
+++ FullStack/FullStack/src/main/java/com/kunal_debug_png/FullStack/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.kunal_debug_png.FullStack.repository;public interface UserRepository {
+package com.kunal_debug_png.FullStack.repository;
+
+import com.kunal_debug_png.FullStack.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
